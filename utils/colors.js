ad39d0b6c24d617ad81453ae96ec906a4b596963
Bhavik Patel <bhavikcpatel@gmail.com>
2018-05-12 17:37:49 -0400

    feat: add app theme colors

---
+++ utils/colors.js
@@ -0,0 +1,8 @@
+export const bgColor = '#FFF3E0';
+export const primaryTextColor = '#666666';
+export const secondaryTextColor = '#aaaaaa';
+export const headerTextColor = '#f4511e';
+export const secondaryBgColor = '#FFE0B2';
+export const secondaryBorderColor = '#f6f7fa';
+export const inputFieldTextColor = '#441608';
+export const placeholderTextColor = '#999';
